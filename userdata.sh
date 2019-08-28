3f53290050ff21c78d0e27f3e80a042e566ab351
vamsikrishnabollina <45496488+vamsikrishnabollina@users.noreply.github.com>
2019-08-28 11:53:07 -0700

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -14,9 +14,5 @@ sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo python get-pip.py
 sudo pip install awscli
 sudo docker volume create jenkins-data
-sudo `aws ecr get-login --registry-ids 498040579653 --no-include-email --region us-west-1`
 sudo docker pull jenkins
-sudo docker pull 498040579653.dkr.ecr.us-west-1.amazonaws.com/xtime-napp:west2jenkins
-#sudo docker run -d --name ejdapi_1.1 -h ejdapi-01 -e override_db_dot_jdbc_dot_url='jdbc:mysql://chatdbprod.cz8wdk3ouu56.us-west-1.rds.amazonaws.com:3306/chatdbprod?useUnicode=true&characterEncoding=UTF-8'  -e override_db_dot_username=chatdbprod -e override_db_dot_password=chatdb1400 -e override_db_dot_jdbc_dot_driver=com.mysql.jdbc.Driver -e override_db_dot_connection_dot_defaultAutoCommit=true -p 8080:8080 860275329454.dkr.ecr.us-east-1.amazonaws.com/xtime/ejdapi:1.1-20180615-16-08-52
-#sudo docker run -d -p 8080:8080 -v jenkins-data:/var/jenkins_home --restart always 498040579653.dkr.ecr.us-west-1.amazonaws.com/xtime-napp:west2jenkins
 sudo docker run -d -p 8080:8080 -v jenkins-data:/var/jenkins_home --restart always docker.io/jenkins:latest
